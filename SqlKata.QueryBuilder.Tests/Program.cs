f3060b4ff16075f345d2375b7b9f9ff78a555713
Chris Eastwood <clipse2g@gmail.com>
2017-10-09 02:36:00 -0400

    apply refactoring to solution
    move directories and rename project files
    
    fix: when mass search and replace goes wrong ;D
    
    modify all namespaces in files
    
    add newlines to end of files

---
+++ SqlKata.QueryBuilder.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿#if NET45
+namespace SqlKata.QueryBuilder.Tests
+{
+    public class Program
+    {
+       static void Main(string[] args)
+       {
+        
+       } 
+    }
+}
+#endif
---
+++ SqlKata.QueryBuilder/Clauses/LimitClause.cs
@@ -0,0 +1,82 @@
+using System.Linq;
+
+namespace SqlKata.QueryBuilder.Clauses
+{
+    public class LimitOffset : AbstractClause
+    {
+        private int _limit;
+        private int _offset;
+
+        public override object[] GetBindings(string engine)
+        {
+            return new[] { _limit, _offset }
+                .Where(x => x > 0)
+                .Cast<object>()
+                .ToArray();
+        }
+
+        public int Limit
+        {
+            get => _limit;
+
+            set
+            {
+                if (value > 0)
+                {
+                    _limit = value;
+                }
+            }
+        }
+
+        public int Offset
+        {
+            get => _offset;
+
+            set
+            {
+                if (value > 0)
+                {
+                    _offset = value;
+                }
+            }
+        }
+
+        public bool HasLimit()
+        {
+            return _limit > 0;
+        }
+
+        public bool HasOffset()
+        {
+            return _offset > 0;
+        }
+
+        public LimitOffset ClearLimit()
+        {
+            _limit = 0;
+            return this;
+        }
+
+        public LimitOffset ClearOffset()
+        {
+            _offset = 0;
+            return this;
+        }
+
+        public LimitOffset Clear()
+        {
+            return ClearLimit().ClearOffset();
+        }
+
+        public override AbstractClause Clone()
+        {
+            return new LimitOffset
+            {
+                Engine = Engine,
+                Offset = Offset,
+                Limit = Limit,
+                Component = Component,
+            };
+        }
+    }
+}
