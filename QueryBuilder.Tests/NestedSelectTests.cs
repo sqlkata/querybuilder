2a05809e31c4848aad6206c723588ada8aa8eed7
Rik Svendsen Rose <r.rose@cubris.dk>
2018-01-22 10:53:23 +0100

    #47 add test for limit on nested commit issue.

---
+++ QueryBuilder.Tests/NestedSelectTests.cs
@@ -0,0 +1,26 @@
+﻿using SqlKata.Compilers;
+using Xunit;
+
+namespace SqlKata.Tests
+{
+    public class NestedSelectTests
+    {
+
+        
+        [Fact]
+        public static void Compile_RawSql_WithLimit_ReturnsCorrectQuery()
+        {
+            var q = new Query().From("Foo as src");
+            var n = new Query();
+            n.From("Bar").Limit(1);
+            n.Select("MyData");
+            q.Select(n, "Bar");
+
+            var target = new SqlServerCompiler();
+
+            var actual = target.Compile(q).RawSql;
+            Assert.Contains(actual, "(SELECT TOP(1) [MyData] FROM BAR)");
+            Assert.DoesNotContain(actual,"(SELECT , [MyData] FROM [Bar])");
+        }
+    }
+}
