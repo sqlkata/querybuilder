41d683e6c7807400ea8b7114eeda7d9bec892bd8
Rik Svendsen Rose <r.rose@cubris.dk>
2018-01-25 16:11:26 +0100

    IENumberable caused multipole execusion and query remade incorrectly

--- QueryBuilder.Tests/NestedSelectTests.cs
+++ QueryBuilder.Tests/NestedSelectTests.cs
@@ -6,21 +6,41 @@ namespace SqlKata.Tests
     public class NestedSelectTests
     {
 
-        
         [Fact]
         public static void Compile_RawSql_WithLimit_ReturnsCorrectQuery()
         {
-            var q = new Query().From("Foo as src");
-            var n = new Query();
-            n.From("Bar").Limit(1);
-            n.Select("MyData");
+            var q = new Query().From("Foo as src").Limit(1);
+            var target = new SqlServerCompiler();
+
+            var actual = target.Compile(q).ToString();
+            Assert.Contains("SELECT TOP (1) * FROM [Foo]",actual);
+        }
+        
+        [Fact]
+        public static void SqlCompile_QueryAadNestedLimit_ReturnsQueryWithTop()
+        {
+            var q = new Query().From("Foo as src").Select("MyData");
+            var n = new Query().From("Bar").Limit(1).Select("MyData");
             q.Select(n, "Bar");
 
             var target = new SqlServerCompiler();
 
-            var actual = target.Compile(q).RawSql;
-            Assert.Contains(actual, "(SELECT TOP(1) [MyData] FROM BAR)");
-            Assert.DoesNotContain(actual,"(SELECT , [MyData] FROM [Bar])");
+            var actual = target.Compile(q).ToString();
+            Assert.Contains("SELECT TOP (1) [MyData] FROM [Bar]",actual);
+            Assert.Contains("SELECT [MyData], (SELECT TOP (1) [MyData] FROM [Bar]) AS [Bar] FROM [Foo] AS [src]",actual);
+        }
+
+        [Fact]
+        public static void SqlCompile_QueryLimitAndNestedLimit_ReturnsQueryWithTop()
+        {
+            var q = new Query().From("Foo as src").Limit(1).Select("MyData");
+            var n = new Query().From("Bar").Limit(1).Select("MyData");
+            q.Select(n, "Bar");
+
+            var target = new SqlServerCompiler();
+
+            var actual = target.Compile(q).ToString();
+            Assert.Contains("SELECT TOP (1) [MyData], (SELECT TOP (1) [MyData] FROM [Bar]) AS [Bar] FROM [Foo] AS [src]",actual);
         }
     }
 }
