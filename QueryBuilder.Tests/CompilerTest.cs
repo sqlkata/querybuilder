1ac9b3c2d2e7a4478ee92fbefc9cdc1fd45d7c9a
Tamas Szenczi <tamas.szenczi@bionical.com>
2018-05-24 17:30:16 +0100

    fix for issue #80 - fix for the issue where the sql parameters are not being cleared after compilation.

---
+++ QueryBuilder.Tests/CompilerTest.cs
@@ -0,0 +1,43 @@
+﻿using SqlKata;
+using SqlKata.Compilers;
+using Xunit;
+
+namespace QueryBuilder.Tests
+{
+    public class CompilerTest
+    {
+        private readonly Compiler pgsql;
+        private readonly MySqlCompiler mysql;
+        private SqlServerCompiler mssql { get; }
+        
+        public CompilerTest()
+        {
+            mssql = new SqlServerCompiler();
+            mysql = new MySqlCompiler();
+            pgsql = new PostgresCompiler();
+        }
+        
+        private string[] Compile(Query q)
+        {
+            return new[]{
+                mssql.Compile(q.Clone()).ToString(),
+                mysql.Compile(q.Clone()).ToString(),
+                pgsql.Compile(q.Clone()).ToString(),
+            };
+        }
+        
+        [Fact]
+        public void Should_clear_query_parameters_after_compilation()
+        {
+            var laptops = new Query("Laptops").Where("Price", ">", 1000).ForPage(3);
+            var laptops2 = new Query("Laptops").Where("Price", ">", 1000).ForPage(4);
+
+            Compile(laptops);
+            Compile(laptops2);
+            
+            Assert.Empty(pgsql.bindings);
+            Assert.Empty(mysql.bindings);
+            Assert.Empty(mssql.bindings);
+        }
+    }
+}
