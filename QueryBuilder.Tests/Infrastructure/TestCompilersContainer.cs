de8000080ef93f6bad90f71d53c8d1bc935cf9f7
Chris Eastwood <clipse2g@gmail.com>
2019-01-18 20:57:52 -0500

    update some xmldoc :grin:

--- QueryBuilder.Tests/Infrastructure/TestCompilersContainer.cs
+++ QueryBuilder.Tests/Infrastructure/TestCompilersContainer.cs
@@ -7,6 +7,12 @@ namespace SqlKata.Tests.Infrastructure
 {
     public class TestCompilersContainer
     {
+        private static class Messages
+        {
+            public const string ERR_INVALID_ENGINECODE = "Engine code '{0}' is not valid";
+            public const string ERR_INVALID_ENGINECODES = "Invalid engine codes supplied '{0}'";
+        }
+
         protected readonly IDictionary<string, Compiler> Compilers = new Dictionary<string, Compiler>
         {
             [EngineCodes.Firebird] = new FirebirdCompiler(),
@@ -23,7 +29,7 @@ namespace SqlKata.Tests.Infrastructure
         }
 
         /// <summary>
-        /// Returns the compiler instance for the given <param name="engineCode"></param>
+        /// Returns a <see cref="Compiler"/> instance for the given engine code
         /// </summary>
         /// <param name="engineCode"></param>
         /// <returns></returns>
@@ -31,7 +37,7 @@ namespace SqlKata.Tests.Infrastructure
         {
             if (!Compilers.ContainsKey(engineCode))
             {
-                throw new InvalidOperationException($"Engine code '{engineCode}' is not valid");
+                throw new InvalidOperationException(string.Format(Messages.ERR_INVALID_ENGINECODE, engineCode));
             }
 
             return Compilers[engineCode];
@@ -50,7 +56,7 @@ namespace SqlKata.Tests.Infrastructure
         }
 
         /// <summary>
-        /// Compiles the query for the given <param name="engineCode"></param>
+        /// Compiles the <see cref="Query"/> against the given engine code
         /// </summary>
         /// <param name="engineCode"></param>
         /// <param name="query"></param>
@@ -61,6 +67,12 @@ namespace SqlKata.Tests.Infrastructure
             return compiler.Compile(query);
         }
 
+        /// <summary>
+        /// Compiles the <see cref="Query"/> against the given engine codes
+        /// </summary>
+        /// <param name="engineCodes"></param>
+        /// <param name="query"></param>
+        /// <returns></returns>
         public TestSqlResultContainer Compile(IEnumerable<string> engineCodes, Query query)
         {
             var codes = engineCodes.ToList();
@@ -72,14 +84,15 @@ namespace SqlKata.Tests.Infrastructure
             if (results.Count != codes.Count)
             {
                 var missingCodes = codes.Where(w => Compilers.All(a => a.Key != w));
-                throw new InvalidOperationException($"Invalid engine codes supplied '{string.Join(", ", missingCodes)}'");
+                var templateArg = string.Join(", ", missingCodes);
+                throw new InvalidOperationException(string.Format(Messages.ERR_INVALID_ENGINECODES, templateArg));
             }
 
             return new TestSqlResultContainer(results);
         }
 
         /// <summary>
-        /// Compiles the query for all known compilers
+        /// Compiles the <see cref="Query"/> against all <see cref="Compiler"/>s
         /// </summary>
         /// <param name="query"></param>
         /// <returns></returns>
