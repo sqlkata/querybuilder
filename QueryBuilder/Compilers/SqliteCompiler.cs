24896b129119beabf25e09f4bb6b36984175904f
Ahmad Moussawi <aymoussawi@gmail.com>
2019-06-28 17:59:38 +0300

    fix Sqlite last_insert_id

--- QueryBuilder/Compilers/SqliteCompiler.cs
+++ QueryBuilder/Compilers/SqliteCompiler.cs
@@ -11,7 +11,7 @@ namespace SqlKata.Compilers
         protected override string parameterPlaceholderPrefix { get; set; } = "@p";
         protected override string OpeningIdentifier { get; set; } = "\"";
         protected override string ClosingIdentifier { get; set; } = "\"";
-        protected override string LastId { get; set; } = "select last_insert_rowid()";
+        protected override string LastId { get; set; } = "select last_insert_rowid() as id";
 
         public override string CompileTrue()
         {
