ae85406aa4bc55f6626089cc72ca24bcd23cee2e
Ahmad Moussawi <aymoussawi@gmail.com>
2019-12-20 14:12:55 +0200

    QueryFactory is Disposable now

--- SqlKata.Execution/QueryFactory.cs
+++ SqlKata.Execution/QueryFactory.cs
@@ -5,7 +5,7 @@ using SqlKata.Compilers;
 
 namespace SqlKata.Execution
 {
-    public class QueryFactory
+    public class QueryFactory : IDisposable
     {
         public IDbConnection Connection { get; set; }
         public Compiler Compiler { get; set; }
@@ -76,6 +76,46 @@ namespace SqlKata.Execution
 
             return compiled;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (this.Connection != null)
+                    {
+                        this.Connection.Dispose();
+                        this.Connection = null;
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~QueryFactory()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
 
     }
 }
