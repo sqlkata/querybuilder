0b356f0184650cea8adcda9fe196e8201c1f1b0d
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-04-27 21:19:35 +0200

    Allow sub queries in inserts alongside parameters

--- QueryBuilder/Query.Insert.cs
+++ QueryBuilder/Query.Insert.cs
@@ -24,7 +24,23 @@ namespace SqlKata
                 throw new InvalidOperationException($"{nameof(columns)} and {nameof(values)} cannot be null or empty");
             }
 
-            if (columnsList.Count != valuesList.Count)
+            var valuesCount = 0;
+            foreach (var value in valuesList)
+            {
+                if (value is Query query && query.Method == "select")
+                {
+                    if (!(query.Clauses[0] is FromClause)) continue;
+                    
+                    valuesCount += query.Clauses.GetRange(1, query.Clauses.Count - 1)
+                        .OfType<Column>()
+                        .Count();
+                }
+                else
+                {
+                    valuesCount++;
+                }
+            }
+            if (columnsList.Count != valuesCount)
             {
                 throw new InvalidOperationException($"{nameof(columns)} and {nameof(values)} cannot be null or empty");
             }
