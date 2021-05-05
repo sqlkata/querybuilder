c6a55f4d093adf06fe23a169739e2069628296c9
Daniël Geelen <daniel.geelen@uipath.com>
2021-05-05 09:46:37 +0200

    Trim trailing whitespace.

--- QueryBuilder/Query.cs
+++ QueryBuilder/Query.cs
@@ -321,7 +321,7 @@ namespace SqlKata
         {
             return Include(relationName, query, foreignKey, localKey, isMany: true);
         }
-        
+
         private static readonly ConcurrentDictionary<Type, PropertyInfo[]> CacheDictionaryProperties = new ConcurrentDictionary<Type, PropertyInfo[]>();
 
         /// <summary>
