ba004ebdff0454c425ec401471c29ff0a483b562
Daniël Geelen <daniel.geelen@uipath.com>
2021-05-05 09:37:01 +0200

    Ensure all files have consistent line-endings.

--- QueryBuilder.Tests/Infrastructure/TestSqlResultContainer.cs
+++ QueryBuilder.Tests/Infrastructure/TestSqlResultContainer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace SqlKata.Tests.Infrastructure
--- QueryBuilder.Tests/OperatorWhitelistTests.cs
+++ QueryBuilder.Tests/OperatorWhitelistTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using SqlKata.Compilers;
 using Xunit;
 
