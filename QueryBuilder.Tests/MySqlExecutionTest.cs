d7a4c587b7b288a407828eb6872005ff18e490c7
Ahmad Moussawi <aymoussawi@gmail.com>
2022-09-09 06:26:35 +0300

    Handle NULL reference error on clone

--- QueryBuilder.Tests/MySqlExecutionTest.cs
+++ QueryBuilder.Tests/MySqlExecutionTest.cs
@@ -54,6 +54,71 @@ namespace SqlKata.Tests
 
             db.Statement("DROP TABLE IF EXISTS `Cars`");
         }
+
+        [Fact]
+        public void Count()
+        {
+            var db = SetupDb();
+            var sql = @"
+                CREATE TABLE Cars(
+                    Id INT PRIMARY KEY AUTO_INCREMENT,
+                    Brand TEXT NOT NULL,
+                    Year INT NOT NULL,
+                    Color TEXT NULL
+                )
+            ";
+            db.Statement(sql);
+
+            db.Statement("INSERT INTO `Cars`(Brand, Year) VALUES ('Honda', 2020)");
+            var count = db.Query("Cars").Count<int>();
+            Assert.Equal(1, count);
+
+            db.Statement("INSERT INTO `Cars`(Brand, Year) VALUES ('Toyota', 2021)");
+            count = db.Query("Cars").Count<int>();
+            Assert.Equal(2, count);
+
+            int affected = db.Query("Cars").Delete();
+            Assert.Equal(2, affected);
+
+            count = db.Query("Cars").Count<int>();
+            Assert.Equal(0, count);
+
+            db.Statement("DROP TABLE IF EXISTS `Cars`");
+        }
+
+        [Fact]
+        public void CloneThenCount()
+        {
+            var db = SetupDb();
+            var sql = @"
+                CREATE TABLE Cars(
+                    Id INT PRIMARY KEY AUTO_INCREMENT,
+                    Brand TEXT NOT NULL,
+                    Year INT NOT NULL,
+                    Color TEXT NULL
+                )
+            ";
+            db.Statement(sql);
+
+            for (int i = 0; i < 10; i++)
+            {
+                db.Query("Cars").Insert(new
+                {
+                    Brand = "Brand " + i,
+                    Year = "2020",
+                });
+            }
+
+            var query = db.Query("Cars").Where("Id", "<", 5);
+            var count = query.Count<int>();
+            var cloneCount = query.Clone().Count<int>();
+
+            Assert.Equal(4, count);
+            Assert.Equal(4, cloneCount);
+
+            db.Statement("DROP TABLE IF EXISTS `Cars`");
+        }
+
         public QueryFactory SetupDb()
         {
             var host = System.Environment.GetEnvironmentVariable("SQLKATA_MYSQL_HOST");
