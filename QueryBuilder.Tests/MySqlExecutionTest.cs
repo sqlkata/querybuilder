9e73d226d1ad4a81dc64a342649c7b793804f08f
Ahmad Moussawi <aymoussawi@gmail.com>
2022-09-10 19:53:37 +0300

    feature_605: move QueryFactory extensions to a separate class

--- QueryBuilder.Tests/MySqlExecutionTest.cs
+++ QueryBuilder.Tests/MySqlExecutionTest.cs
@@ -181,19 +181,4 @@ namespace SqlKata.Tests
 
 
     }
-    static class QueryFactoryExtensions
-    {
-        public static QueryFactory Create(this QueryFactory db, string table, IEnumerable<string> cols)
-        {
-            db.Drop(table);
-            db.Statement($"CREATE TABLE `{table}`({string.Join(", ", cols)})");
-            return db;
-        }
-
-        public static QueryFactory Drop(this QueryFactory db, string table)
-        {
-            db.Statement($"DROP TABLE IF EXISTS `{table}`");
-            return db;
-        }
-    }
 }
